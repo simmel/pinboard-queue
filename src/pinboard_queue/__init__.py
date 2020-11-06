43c47ec0c8fb327855a1a99030b283bc8c6a3c39
Simon Lundström <github-commits@soy.se>
2020-11-06 06:59:10 +0100

    Add amqp url pinboard api token options

--- src/pinboard_queue/__init__.py
+++ src/pinboard_queue/__init__.py
@@ -6,8 +6,13 @@ import click
 
 
 @click.command()
-def main():
-    print("lol {}".format(name))
+@click.option("--amqp-url", required=True, show_envvar=True, help="URL to AMQP server")
+@click.option(
+    "--pinboard-api-token", required=True, show_envvar=True, help="Pinboard API token"
+)
+def main(*, amqp_url: str, pinboard_api_token: str):
+    """A Pinboard.in feed to Message Queue doer"""
+    print("{!r} {!r}".format(amqp_url, pinboard_api_token))
 
 
 main(auto_envvar_prefix="PINQUE")
