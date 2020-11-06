e091dd16dd320b8da56b975547c83d7cb9132833
Simon Lundström <github-commits@soy.se>
2020-11-06 06:31:16 +0100

    Convert to click

--- src/pinboard_queue/__init__.py
+++ src/pinboard_queue/__init__.py
@@ -1,8 +1,13 @@
 __version__ = "0.1.0"
 
+import typing
 
+import click
+
+
+@click.command()
 def main():
-    print("lol")
+    print("lol {}".format(name))
 
 
-main()
+main(auto_envvar_prefix="PINQUE")
