ebb126ae5e131a2a9c2b706fde3475818d9b1780
Simon Lundström <github-commits@soy.se>
2021-04-27 18:50:53 +0200

    AMQP 1.0 - Ain't nobody got time for that!

--- src/pinboard_queue/__init__.py
+++ src/pinboard_queue/__init__.py
@@ -4,8 +4,6 @@ import logging
 import typing
 
 import click
-import uamqp  # type: ignore
-from uamqp import authentication
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
@@ -18,25 +16,7 @@ log = logging.getLogger(__name__)
 )
 def main(*, amqp_url: str, pinboard_api_token: str):
     """A Pinboard.in feed to Message Queue doer"""
-    url = uamqp.compat.urlparse(amqp_url)
-    plain_auth = authentication.SASLPlain(
-        hostname=url.hostname,
-        username=url.username,
-        password=url.password,
-        encoding="UTF-8",
-    )
-    target = "queue://{}/".format(url.path)
-    send_client = uamqp.SendClient(target, auth=plain_auth, debug=False)
-    header = uamqp.message.MessageHeader()
-    header.durable = True
-    msg_content = b"hello world"
-    message = uamqp.Message(
-        msg_content,
-        header=header,
-    )
-    send_client.queue_message(message)
-    results = send_client.send_all_messages()
-    print("Message sent: {!r}".format(results))
+    print(amqp_url)
 
 
 main(auto_envvar_prefix="PINQUE")
